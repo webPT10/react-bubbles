de44cb74e1a0054054bcd54c07b81a1cab3d0351
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-24 17:11:18 -0800

    ColorList.js -- set up blank .PUT() request

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utilities/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -21,6 +21,10 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put()
+      .then()
+      .catch()
   };
 
   const deleteColor = color => {
