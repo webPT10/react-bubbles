df843446d440ec3bce8150031587c32cff10824f
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-01-24 16:26:23 -0800

    set up useState on Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+  
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
